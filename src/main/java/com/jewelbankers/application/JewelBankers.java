c4e7245f1c0f73081d0cb32ceb30d7f84d0e5234
iamdeepak2005 <166904926+iamdeepak2005@users.noreply.github.com>
2024-10-15 12:13:57 +0530

    my changes to pull

--- src/main/java/com/jewelbankers/application/JewelBankers.java
+++ src/main/java/com/jewelbankers/application/JewelBankers.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @ComponentScan({ "com" })
 //@OpenAPIDefinition(info = @Info(title = "Jewel Bankers Api",version = "V.1.0",description = "Documentation for Jewel Bankers APIs"))
 
-public class JewelBankers {	
+public class JewelBankers {			
 	
 	/**
 	 * @return	
