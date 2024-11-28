a63c9a86e010127692d85e802245c69813a4b003
roshankag <roshankag2003@gmail.com>
2024-11-29 00:52:59 +0530

    Added SSL for Ec2

---
+++ src/main/java/com/jewelbankers/configuration/SSLConfig.java
@@ -0,0 +1,24 @@
+package com.jewelbankers.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class SSLConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> redirectHttpToHttps() {
+        return factory -> factory.addAdditionalTomcatConnectors(createHttpConnector());
+    }
+
+    private org.apache.catalina.connector.Connector createHttpConnector() {
+        org.apache.catalina.connector.Connector connector = new org.apache.catalina.connector.Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080); // HTTP port
+        connector.setSecure(false);
+        connector.setRedirectPort(443); // Redirect to HTTPS port
+        return connector;
+    }
+}
